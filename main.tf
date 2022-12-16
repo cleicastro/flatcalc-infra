4333013b2c8ce772f6838e039019faff3792d97b
Clei Castro <cleicastro.ti@hotmail.com>
2022-12-16 12:17:52 -0300

    create hcl to prod

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "tfstate-flatcalc-263822963588"
-    key            = "prod/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "flatcalc-terraform-prod"
-  }
+  backend "s3" {}
 }
---
+++ prod_main.hcl
@@ -0,0 +1,4 @@
+bucket         = "tfstate-flatcalc-263822963588"
+key            = "prod/terraform.tfstate"
+region         = "us-east-1"
+dynamodb_table = "flatcalc-terraform-prod"
